3965b9a1b67cc4fa73241ab63bec0918fe53f1c5
unknown <tgm13ugu@SSF-NBS-004041.UEA.AC.UK>
2017-10-06 16:44:01 +0100

    Added tibbles

---
+++ fragments/tibbles.R
@@ -0,0 +1,22 @@
+# Creating dataframes with tibble
+# Need to use dplyr
+library(dplyr)
+
+#Instantiate the data frame
+mydataframe <- data.frame()
+
+row1 <- tibble( column1 = "1",
+                column2 = "This is the first row")
+
+row2 <- tibble( column1 = "2",
+                column2 = "This is the first row")
+
+mydataframe <- bind_rows(row1,row2)
+
+# print the contents of this data frame in the console
+# Should show something like this (row numbers ignored) :
+# # A tibble: 2 x 2
+# column1                 column2
+# <chr>                     <chr>
+#     1   1 This is the first row
+#     2   2 This is the first row
