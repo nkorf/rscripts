b7c733abde0403d69848e1212b558ba623ec56d3
Nikos Korfiatis <nikos.korfiatis@gmail.com>
2018-06-08 00:43:13 +0100

    Update listfiles.R

--- fragments/listfiles.R
+++ fragments/listfiles.R
@@ -1,1 +1,12 @@
-# Code
+# Code to list files in a directory 
+
+#File directory location can be specified as a separate variable
+
+file_list <- list.files("~/Dropbox/")
+
+# Usefull info the ~ (pronounced as tidle) specificies the home 
+# path in UNIX like systems 
+
+# For Windows Systems the C:\Users\username is the equivalent directory
+
+file_list 
